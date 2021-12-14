f7c672a0fd0801e9318fb035c1847fe17c02c123
Piyali Adhikary <piyali115@gmail.com>
2021-12-14 19:31:05 +0530

    Initial commit

---
+++ app/src/main/java/com/piyali/retailapplication/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.piyali.retailapplication.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
